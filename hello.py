8922ddd232650aa06ad7406e16c9966cc4d40d44
sbi0402 <102271690+sbi0402@users.noreply.github.com>
2022-03-31 11:40:53 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,13 +3,6 @@
 // Second: Local respository modification
 print("hello world")
 print("Tell your world")
-=======
-// For command line git tutorial
-// First: GitHub modification
-// Second: Local respository modification
-print("hello world")
-print("Tell your world")
->>>>>>> origin/masterf
 print("Tell his world")
 print("Tell her world")
 

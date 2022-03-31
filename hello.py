6634989a62bd2a95ba827a782702970aec848768
buil suk <sukbuil123@naver.com>
2022-03-31 11:16:23 +0900

    hello.py modified on Local respository

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
-print("hello world")
 
+// For command line git tutorial
+// Local respository modification
+
+print("hello world")
+print("Tell your world")
+print("Tell his world")
+print("Tell her world")
 
